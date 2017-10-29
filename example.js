074bf75c00b7395835a5891f5b98420c8bcce4af
Jisang you <jisangman89@gmail.com>
2017-10-29 16:20:58 +0900

    first commit

---
+++ example.js
@@ -0,0 +1,5 @@
+document.write("환영합니다. 자바스크립트 세계로 온것을!");
+document.write("자바스크립트는 대소문자 구별");
+document.write("실행문 마치고, 한줄에 2개의 실행문을 작성!");
+document.write("한줄에 하나씩만!");
+document.write("큰따옴표와 작은 따옴표 오류 조심!");
