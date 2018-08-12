ca6f3602bfd348024d0c32cdb1af105ea8bcf3d0
youjisang <jisangman89@gmail.com>
2018-08-12 22:29:54 +0900

    2018/08/12

---
+++ project/shopping_re/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var admin = require('./routes/admin');
+
+var app = express();
+var port = 3000;
+
+app.get('/', function (req, res) {
+    res.send('first app');
+});
+
+app.use('/admin', admin);
+
+app.listen(port, function () {
+    console.log('Express listening on port', port);
+})
