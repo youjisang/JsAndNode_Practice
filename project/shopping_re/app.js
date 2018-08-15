da2d72d693bdf043a700e52870ac22215185ee2e
youjisang <jisangman89@gmail.com>
2018-08-15 21:31:24 +0900

    2018/08/15

--- project/shopping_re/app.js
+++ project/shopping_re/app.js
@@ -1,22 +1,31 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
 var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var autoIncrement = require('mongoose-auto-increment');
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function(){
     console.log("mongodb connect");
 });
 
-var connect = mongoose.connect('mongodb://127.0.0.1:27017/fastcampus', { useMongoClient: true });
+var connect = mongoose.connect('mongodb://127.0.0.1:27017/newProject', { useMongoClient: true });
 autoIncrement.initialize(connect);
 
 var admin = require('./routes/admin');
 
 var app = express();
 var port = 3000;
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+// 확장자가 ejs 로 끈나는 뷰 엔진을 추가한다.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.send('first app');
--- project/shopping_re/routes/admin.js
+++ project/shopping_re/routes/admin.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var ProductsModel = require('../models/ProductsModel');
 var router = express.Router();
 
 router.get('/', function (req, res) {
     res.send('admin app');
 });
-router.get('/products', function (req, res) {
-    res.render('admin/products', {
-            message: "hello"
-        } // message 란 변수를 템플릿으로 내보낸다.
-    );
+router.get('/products', function(req,res){
+    ProductsModel.find(function(err,products){
+        res.render( 'admin/products' , 
+            { products : products } // DB에서 받은 products를 products변수명으로 내보냄
+        );
+    });
+});
+router.get('/products/write', function(req,res){
+    res.render( 'admin/form');
+});
+router.post('/products/write', function(req,res){
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    });
+    product.save(function(err){
+        res.redirect('/admin/products');
+    });
 });
 module.exports = router;
